62d023df98989b70c5df148fad6fca3e600bb063
hansoo280102 <tantran012080@gmail.com>
2025-01-22 20:33:12 +0700

    113

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -12,7 +12,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-import vn.hoidanit.laptopshop.service.validator.StrongPassword;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
